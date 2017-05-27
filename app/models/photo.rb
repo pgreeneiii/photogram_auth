63dbeb6475003567e6900a0e69d9cf2685123714
Phil Greene <pgreeneiii@users.noreply.github.com>
2017-05-27 15:00:50 -0500

    Formatting for Photos Index in Progress

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,9 @@
 class Photo < ApplicationRecord
    validates :user_id, :presence => true
 
-   has_many(:commments, :class_name => "Comment", :foreign_key => "photo_id")
+   belongs_to(:user, :class_name => "User", :foreign_key => "user_id")
+
+   has_many(:comments, :class_name => "Comment", :foreign_key => "photo_id")
    has_many(:likes, :class_name => "Like", :foreign_key => "photo_id")
    has_many :fans, :through => :likes, :source => :user
 end
